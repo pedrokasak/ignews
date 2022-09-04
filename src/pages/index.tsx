bc625dc6ad3ca5aa726d788dedad895d560c8b2c
Pedro Henrique <pedrohesm@gmail.com>
2022-09-04 19:19:02 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/globals.module.scss'
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+          <title>Inicio| ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World
+      </h1>
+    </>
+  )
+}
